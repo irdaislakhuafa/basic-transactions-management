0f51f50c637b2e48e9dab0b104d4e2e493470692
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2021-12-05 02:02:03 +0700

    First commit and add config for database connection

---
+++ src/main/java/com/basic/transactions/management/basictransactionsmanagement/BasicTransactionsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.basic.transactions.management.basictransactionsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTransactionsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTransactionsManagementApplication.class, args);
+	}
+
+}
